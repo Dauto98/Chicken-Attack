e6d06f449389c7a911e3f48aac0d51ba6ea5353f
Dauto98 <dauto98@gmail.com>
2017-02-13 16:36:25 +0700

    add background and divider

--- js/main.js
+++ js/main.js
@@ -24,18 +24,28 @@ var preload = function(){
   Nakama.game.time.advancedTiming = true;
 
   Nakama.game.load.atlasJSONHash('assets', 'Assets/assets.png', 'Assets/assets.json');
-  Nakama.game.load.image('background', 'Assets/Map1.png');
+  Nakama.game.load.image('gray_background', 'Assets/gray_background.png');
+  Nakama.game.load.image('pink_background', 'Assets/pink_background.png');
+
 }
 
 // initialize the game
 var create = function(){
   Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
   Nakama.keyboard = Nakama.game.input.keyboard;
+
+  Nakama.backgroundLeft = Nakama.game.add.tileSprite(0, 0, 320, 960, "gray_background");
+  Nakama.backgroundRight = Nakama.game.add.tileSprite(320, 0, 320, 960, "pink_background");
+
+  Nakama.graph = Nakama.game.add.graphics(Nakama.game.width/2, 0);
+  Nakama.graph.lineStyle(5, 0xECEFF1);
+  Nakama.graph.lineTo(0, Nakama.game.height);
 }
 
 // update game state each frame
 var update = function(){
-
+  Nakama.backgroundLeft.tilePosition.y += 1;
+  Nakama.backgroundRight.tilePosition.y += 1;
 }
 
 // before camera render (mostly for debug)
